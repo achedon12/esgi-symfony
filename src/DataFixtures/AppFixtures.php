5a4c97c00603d7bae88b2e8ad86ee549d1952602
leo <leo@confluent-digital.com>
2024-12-19 11:46:05 +0100

    feat: admin page

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,10 +5,13 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+
+    private const NUMBER_OF_USERS = 50;
     public function __construct(
         private readonly UserPasswordHasherInterface $hasher
     )
@@ -17,21 +20,26 @@ class AppFixtures extends Fixture
 
     public function load(ObjectManager $manager): void
     {
-        $this->loadUsers($manager);
+        $faker = Factory::create();
+        $this->loadUsers($manager, $faker);
         $manager->flush();
     }
 
-    private function loadUsers(ObjectManager $manager): void
+    private function loadUsers(ObjectManager $manager, $faker): void
     {
-        $regularUser = new User();
-        $regularUser
-            ->setEmail('regular@email.com')
-            ->setPassword($this->hasher->hashPassword($regularUser, 'regular'));
-        $manager->persist($regularUser);
+        // create regular users
+        for($i = 0; $i < self::NUMBER_OF_USERS; $i++) {
+            $regularUser = new User();
+            $regularUser
+                ->setEmail($faker->email())
+                ->setPassword($this->hasher->hashPassword($regularUser, 'regular'));
+            $manager->persist($regularUser);
+        }
 
+        // create admin user
         $adminUser = new User();
         $adminUser
-            ->setEmail('admin@email.com')
+            ->setEmail($faker->email())
             ->setPassword($this->hasher->hashPassword($adminUser, 'admin'))
             ->setRoles(['ROLE_ADMIN']);
         $manager->persist($adminUser);
