4cc08866192f5ce1a8969e6e7edc2faf7cd1683d
leo <leo@confluent-digital.com>
2024-12-19 17:52:55 +0100

    feat: send message

--- src/Controller/DiscussionController.php
+++ src/Controller/DiscussionController.php
@@ -7,6 +7,7 @@ use App\Entity\Message;
 use App\Repository\DiscussionRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -16,10 +17,16 @@ use Symfony\Component\Routing\Attribute\Route;
 class DiscussionController extends AbstractController
 {
 
-    #[Route('/{id}', name: 'index')]
-    public function index(Discussion $discussion, DiscussionRepository $discussionRepository): Response
+    #[Route('/{id}', name: 'index', requirements: ['id' => '\d+'])]
+    public function index(string $id, DiscussionRepository $discussionRepository): Response
     {
         $user = $this->getUser();
+        $discussion = $discussionRepository->find($id);
+
+        if (!$discussion) {
+            throw $this->createNotFoundException('Discussion not found.');
+        }
+
         $discussions = $discussionRepository->findByUser($user);
 
         return $this->render('discussion/index.html.twig', [
@@ -33,10 +40,19 @@ class DiscussionController extends AbstractController
     public function send(Request $request, DiscussionRepository $discussionRepository, EntityManagerInterface $entityManager): Response
     {
         $user = $this->getUser();
+
         $content = $request->request->get('message');
         $discussionId = $request->request->get('discussion_id');
 
+        if (empty($content) || empty($discussionId)) {
+            $this->addFlash('error', 'Le message ou la discussion est invalide.');
+            return $this->redirectToRoute('app_home_index');
+        }
+
         $discussion = $discussionRepository->find($discussionId);
+        if (!$discussion) {
+            throw $this->createNotFoundException('Discussion introuvable.');
+        }
 
         $message = new Message();
         $message->setAuthor($user)
@@ -44,14 +60,12 @@ class DiscussionController extends AbstractController
             ->setDiscussion($discussion)
             ->setCreationDate(new \DateTimeImmutable());
 
+        $discussion->setUpdateDate(new \DateTimeImmutable());
+
         $entityManager->persist($message);
-
-        $discussion->setUpdateDate(new \DateTimeImmutable())
-            ->addMessage($message);
-
-        $entityManager->persist($discussion);
         $entityManager->flush();
 
         return $this->redirectToRoute('app_discussion_index', ['id' => $discussionId]);
     }
+
 }
