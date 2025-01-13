90ab1a5a6249cdd709415f2569546f71418257db
leo <leo@confluent-digital.com>
2025-01-13 12:46:14 +0100

    feat: edit account

--- src/Form/UpdateUserFormType.php
+++ src/Form/UpdateUserFormType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\User;
 use App\Enum\GenderEnum;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
@@ -28,19 +29,34 @@ class UpdateUserFormType extends AbstractType
                 'mapped' => false,
                 'required' => false,
             ])
-            ->add('longitude', TextType::class, [
-                'label' => 'Longitude',
-            ])
-            ->add('latitude', TextType::class, [
-                'label' => 'Latitude',
-            ])
             ->add('birthdate', DateType::class, [
                 'widget' => 'single_text',
                 'label' => 'Birthdate',
             ])
             ->add('bio', TextType::class, [
                 'label' => 'Bio',
+                'required' => false,
             ])
+            ->add('interests', CollectionType::class, [
+                'entry_type' => TextType::class,
+                'entry_options' => ['label' => false],
+                'allow_add' => true,
+                'allow_delete' => true,
+                'by_reference' => false,
+                'label' => 'Interests',
+                'required' => false,
+            ])
+            ->add('longitude', TextType::class, [
+                'label' => 'Longitude',
+                'required' => false,
+                'attr' => ['style' => 'display: none;'],
+            ])
+            ->add('latitude', TextType::class, [
+                'label' => 'Latitude',
+                'required' => false,
+                'attr' => ['style' => 'display: none;'],
+            ])
+
             ->add('gender', ChoiceType::class, [
                 'choices' => [
                     'Male' => 'male',
