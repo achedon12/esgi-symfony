f6f541a57f95e9b325800fee0a2a1b72b1df0f8a
leo <leo@confluent-digital.com>
2025-01-14 12:12:12 +0100

    feat: language switcher

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use App\Entity\Discussion;
 use App\Entity\Like;
 use App\Entity\Message;
+use App\Entity\Offer;
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
@@ -22,22 +23,6 @@ class DashboardController extends AbstractDashboardController
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        //return parent::index();
-
-        // Option 1. You can make your dashboard redirect to some common page of your backend
-        //
-        //$adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
-        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
-
-        // Option 2. You can make your dashboard redirect to different pages depending on the user
-        //
-        // if ('jane' === $this->getUser()->getUsername()) {
-        //     return $this->redirect('...');
-        // }
-
-        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
-        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
-        //
         return $this->render('admin/dashboard.html.twig');
     }
 
@@ -57,6 +42,7 @@ class DashboardController extends AbstractDashboardController
             MenuItem::linkToCrud('Discussions', 'fas fa-comments', Discussion::class)->setController(DiscussionCrudController::class),
             MenuItem::linkToCrud('Messages', 'fas fa-envelope', Message::class)->setController(MessageCrudController::class),
             MenuItem::linkToCrud('Likes', 'fas fa-heart', Like::class)->setController(LikeCrudController::class),
+            MenuItem::linkToCrud('Offers', 'fas fa-handshake', Offer::class)->setController(OfferCrudController::class),
         ];
     }
 }
