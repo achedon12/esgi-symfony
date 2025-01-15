79258cc1f3e160edd1758f2d042415b0e3b2f974
IPandragonI <mathysf84pro@gmail.com>
2025-01-15 08:20:14 +0100

    feat: User profile picture and additive pictures management

---
+++ migrations/Version20250114125630.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250114125630 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user ADD additive_images JSON DEFAULT NULL COMMENT \'(DC2Type:json)\', CHANGE offer_id offer_id INT NOT NULL, CHANGE image image VARCHAR(255) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user DROP additive_images, CHANGE offer_id offer_id INT DEFAULT 1, CHANGE image image JSON NOT NULL COMMENT \'(DC2Type:json)\'');
+    }
+}
