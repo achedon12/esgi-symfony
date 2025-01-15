11bb07899f4dd5633bbbaf85464faf7555ca59c2
leo <leo@confluent-digital.com>
2025-01-15 08:54:40 +0100

    fix: migration

--- migrations/Version20250114125630.php
+++ migrations/Version20250114125630.php
@@ -20,12 +20,12 @@ final class Version20250114125630 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user ADD additive_images JSON DEFAULT NULL COMMENT \'(DC2Type:json)\', CHANGE offer_id offer_id INT NOT NULL, CHANGE image image VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE user ADD additive_images JSON DEFAULT NULL COMMENT \'(DC2Type:json)\', CHANGE image image VARCHAR(255) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user DROP additive_images, CHANGE offer_id offer_id INT DEFAULT 1, CHANGE image image JSON NOT NULL COMMENT \'(DC2Type:json)\'');
+        $this->addSql('ALTER TABLE user DROP additive_images, CHANGE image image JSON NOT NULL COMMENT \'(DC2Type:json)\'');
     }
 }
